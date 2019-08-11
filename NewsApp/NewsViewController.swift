729ce28a1298453074946274c69058373b268dbd
“yuriko0922” <yrk922@gmail.com>
2019-08-11 14:43:07 +0900

    解析処理

--- NewsApp/NewsViewController.swift
+++ NewsApp/NewsViewController.swift
@@ -8,30 +8,145 @@
 
 import UIKit
 import XLPagerTabStrip
+import WebKit
 
-class NewsViewController: UIViewController,IndicatorInfoProvider {
+class NewsViewController: UIViewController,IndicatorInfoProvider,UITableViewDataSource,UITableViewDelegate,WKNavigationDelegate,XMLParserDelegate {
+    
+    //テーブルビューのインスタンス取得
+    var tableView: UITableView = UITableView()
+    //XMLParserインスタンス
+    var parser = XMLParser()
+    //記事情報の入れ物
+    var articles:[Any] = []
+    
+    
+    @IBOutlet weak var WebView: WKWebView!
+    
+    
+    @IBOutlet weak var Toolber: UIToolbar!
+    
     var url: String = ""
+    
     
     var itemInfo: IndicatorInfo = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        //デリゲートとの接続
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //parserと
+        parser.delegate = self
+        
+        tableView.frame = CGRect(x: 0, y: 50, width: self.view.frame.width, height: self.view.frame.height)
+        
+        //navigationDelegateとの接続
+        WebView.navigationDelegate = self
+        
+        //tableViewをviewに追加
+        self.view.addSubview(tableView)
+        
+        //最初は隠す
+        WebView.isHidden = true
+        Toolber.isHidden = true
         // Do any additional setup after loading the view.
     }
+    
+    
+    func parserUrl(){
+        let urlToSend: URL = URL(string: url)!
+        parser = XMLParser(contentsOf: urlToSend)!
+        articles = []
+        //解析の実行
+        parser.parse()
+        //tableViewのリロード
+        tableView.reloadData()
+    }
+    
+    
+    //セルの高さ
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    //セルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //記事の数だけセルを返す
+        return articles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+        
+        //セルの色
+        cell.backgroundColor = #colorLiteral(red: 0.9006147981, green: 0.8838652968, blue: 1, alpha: 1)
+        
+        //記事テキストフォントとテキストサイズの設定
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        //テキストの色
+        cell.textLabel?.textColor = UIColor.black
+        
+        //記事のurlのサイズとフォントと色
+        cell.detailTextLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        cell.detailTextLabel?.textColor = UIColor.gray
+        
+        
+        return cell
+        
+    }
+    
+    //セルをタップした時の処理
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        //tableViewを隠す
+        tableView.isHidden = true
+        //Toolberを表示する
+        Toolber.isHidden = false
+        //WebView表示するを表示する
+        WebView.isHidden = false
+    }
+    
+    //キャンセル
+    @IBAction func cancel(_ sender: Any) {
+        //tableViewを表示する
+        tableView.isHidden = false
+        //Toolberを隠す
+        Toolber.isHidden = true
+        //WebView表示するを隠す
+        WebView.isHidden = true
+        
+    }
+    //戻る
+    @IBAction func backPage(_ sender: Any) {
+        WebView.goBack()
+    }
+    //次へ
+    @IBAction func nextpage(_ sender: Any) {
+        WebView.goForward()
+    }
+    //リロード
+    @IBAction func refreshPage(_ sender: Any) {
+        WebView.reload()
+    }
+    
+    
     
     func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
         
         return itemInfo
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
